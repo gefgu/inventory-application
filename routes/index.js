0679e62be15df9be846f6242e785c33e57df70e0
gefgu <gefgu@hotmail.com>
2022-07-10 16:53:44 -0300

    Add routes without implementation

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,31 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+// Item Routes
+router.get("/item/create", item_controller.item_create_get);
+router.post("/item/create", item_controller.item_create_post);
+
+router.get("/item/:id/delete", item_controller.item_delete_get);
+router.post("/item/:id/delete", item_controller.item_delete_post);
+
+router.get("/item/:id/update", item_controller.item_update_get);
+router.post("/item/:id/update", item_controller.item_update_post);
+
+router.get("/item/:id", item_controller.item_detail);
+router.get("/items", item_controller.item_list);
+
+
+// Category Routes
+router.get("/category/create", category_controller.category_create_get);
+router.post("/category/create", category_controller.category_create_post);
+
+router.get("/category/:id/delete", category_controller.category_delete_get);
+router.post("/category/:id/delete", category_controller.category_delete_post);
+
+router.get("/category/:id/update", category_controller.category_update_get);
+router.post("/category/:id/update", category_controller.category_update_post);
+
+router.get("/category/:id", category_controller.category_detail);
+router.get("/categorys", category_controller.category_list);
+
 module.exports = router;
